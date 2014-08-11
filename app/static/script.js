d5b6bf7ec84c25e36548d1ca2ef10bfdff800234
David <David@davids-mbp.home>
2014-08-11 18:21:42 -0400

    init

--- app/static/script.js
+++ app/static/script.js
@@ -24,8 +24,37 @@ $(document).ready(function(){
 			case "Italian":
 				$("#rlist").html("");
 				$("#rlist").append('<li>Marea</li>');
+				// $("#rlist").append('<li>Carbone</li>');
+				
 				break;
 		};
 
 	});
 	});
+
+
+
+$(document).ready(function () {
+    $("#button1").click(function () {
+        $("#list1 > option:selected").each(function () {
+            $(this).remove().appendTo("#list2");
+            rearrangeList("#list2");
+        });
+    });
+
+    $("#button2").click(function () {
+        $("#list2 > option:selected").each(function () {
+            $(this).remove().appendTo("#list1");
+            rearrangeList("#list1");
+        });
+    });
+
+function byValue(a, b) {
+    return a.value > b.value ? 1 : -1;
+};
+
+function rearrangeList(list) {
+    $(list).find("option").sort(byValue).appendTo(list);
+};
+    
+});
