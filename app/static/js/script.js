1a81fbb42ab7e183337cb3a594a077c11b8af17e
David Kim <kimd83@gmail.com>
2014-12-28 22:23:01 -0500

    change display

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,18 +1,60 @@
+$(function() {
+    $( "#start_date, #end_date" ).datepicker({
+        dateFormat: "mm/dd/yy"
+    }).datepicker("setDate","0");
+});
+
+$(function(){
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1;
+  var yyyy = today.getFullYear();
+
+  if(dd<10){
+    dd = '0'+dd;
+  }
+  if(mm<10){
+    mm = '0'+mm;
+  }
+  today_string = mm + '/'+dd+'/'+yyyy;
+
+  if(localStorage.getItem("time") !=null){
+    $("#time").val(localStorage.getItem('time'));
+  };
+  if(localStorage.getItem("people") !=null){
+    $("#people").val(localStorage.getItem('people'));
+  };
+
+  var start_date = new Date(localStorage.getItem("start_date"));
+  if(localStorage.getItem("start_date") !=null){
+    $("#start_date").val(localStorage.getItem('start_date'));
+  };
+  if(start_date < today){
+    $("#start_date").val(today_string);
+  };
+
+  var end_date = new Date(localStorage.getItem("end_date"));
+  if(localStorage.getItem("end_date") !=null){
+    $("#end_date").val(localStorage.getItem('end_date'));
+  };
+  if(end_date < today){
+    $("#end_date").val(today_string);
+  };
+
+  if(localStorage.getItem('selected') !=null){
+    var restaurants = document.getElementById("restaurants");
+    for (var i=0;i<restaurants.options.length;i++){
+        restaurants.options[i].removeAttribute("selected","selected");
+      if(localStorage.getItem('selected').split(",").indexOf(restaurants.options[i].value)>-1){
+        restaurants.options[i].setAttribute("selected","selected");
+      }
+    }
+  };
+});
+
 $(function()
 {
     $("#restaurants").pickList();
-});
-
-$(function() {
-    $( "#start_date" ).datepicker({
-        dateFormat: "mm/dd/yy"
-    }).datepicker("setDate","0");
-});
-
-$(function() {
-    $( "#end_date" ).datepicker({
-        dateFormat: "mm/dd/yy"
-    }).datepicker("setDate","0");
 });
 
 function loadData(){
@@ -29,37 +71,34 @@ function loadData(){
     var time = document.getElementById("time").value;
     var people = document.getElementById("people").value;
     var data = {selected: selected, start_date: start_date, end_date: end_date, time: time, people: people};
-        
-    // var page_url = "http://www.opentable.com"
-    // var page_url = 'http://www.opentable.com/opentables.aspx?t=rest&r=restaurant_id&m=8&p=people&d=1/7/2015%206:00:00%20PM&scpref=100'
+    localStorage.clear();
+    localStorage.setItem('time', time);
+    localStorage.setItem('people', people);
+    localStorage.setItem('start_date', start_date);
+    localStorage.setItem('end_date', end_date);
+    localStorage.setItem('selected', selected);
+
     var request = $.ajax({
         type: form.prop('method'),
         url: window.location.protocol+'//'+window.location.host+"/crossdomain",
         data: JSON.stringify(data),
         success: function( response ) {
           var result = response;
-          // $("#tables").append('<li>' + selected + '</li>');
-          // $("#tables").append('<li>' + start_date + '</li>');
-          // $("#tables").append('<li>' + end_date + '</li>');
-          // $("#tables").append('<li>' + time + '</li>');
-          // $("#tables").append('<li>' + people + '</li>');
           var result = $.parseJSON(result);
-          console.log(result);
           $("#tables").empty();
           for(var key in result){
-            console.log(key);
-            console.log(result[key]);
-            console.log(result[key][1])
-            $("#tables").append('<li>' + key + '</li>');
+            $("#tables").append('<br><b>' + key + '</b><br>');
+            if(result[key].length<1){
+              $("#tables").append('<li>No Available Tables</li>');
+            }
+            else{
             for(var i=0;i<result[key].length;i++){
                 $("#tables").append('<li><a href='+ result[key][i].split(" ")[2] + ">" + result[key][i].split(" ")[0] + " " + result[key][i].split(" ")[1] + '</a></li>');
             }
+          }
         }
         }
     });
-    
-    // request.done(function(reply){
-    //     $('#tables').append(reply.value);
     return false;
 };
 
