7729787b17e53dd736e6ce3eec9a743c0197ed37
David Kim <kimd83@gmail.com>
2015-01-10 18:32:58 -0500

    send an email alert when a table becomes available

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -5,9 +5,11 @@ $(function(){
 });
 
 $(function(){
-
-  if(localStorage.getItem("time") != null){
-    $("#time").val(localStorage.getItem('time'));
+  if(localStorage.getItem("start_time") != null){
+    $("#start_time").val(localStorage.getItem('start_time'));
+  };
+  if(localStorage.getItem("end_time") != null){
+    $("#end_time").val(localStorage.getItem('end_time'));
   };
   if(localStorage.getItem("people") != null){
     $("#people").val(localStorage.getItem('people'));
@@ -17,7 +19,6 @@ $(function(){
     var dd = date.getDate()
     var mm = date.getMonth() + 1;
     var yyyy = date.getFullYear();
-  
     if(dd<10){
       dd = '0'+dd;
     }
@@ -48,7 +49,7 @@ $(function(){
   };
 
   if(localStorage.getItem('selected') != null){
-    var restaurants = document.getElementById("restaurants");
+    var restaurants = $("#restaurants")[0];
     for(var i=0; i<restaurants.options.length; i++){
       restaurants.options[i].removeAttribute("selected","selected");
       if(localStorage.getItem('selected').split(",").indexOf(restaurants.options[i].value)>-1){
@@ -64,11 +65,12 @@ $(function(){
 
 function loadData(){
   var form = $(this);
-  var start_date = document.getElementById("start_date").value;
-  var end_date = document.getElementById("end_date").value;
-  var time = document.getElementById("time").value;
-  var people = document.getElementById("people").value;
-  var restaurants = document.getElementById("restaurants");
+  var start_date = $("#start_date")[0].value;
+  var end_date = $("#end_date")[0].value;
+  var start_time = $("#start_time")[0].value;
+  var end_time = $("#end_time")[0].value;
+  var people = $("#people")[0].value;
+  var restaurants = $("#restaurants")[0];
   var selected = [];
   for (var i=0; i<restaurants.options.length; i++){
     if(restaurants.options[i].selected == true){
@@ -78,11 +80,11 @@ function loadData(){
   localStorage.clear();
   localStorage.setItem('start_date', start_date);
   localStorage.setItem('end_date', end_date);
-  localStorage.setItem('time', time);
+  localStorage.setItem('start_time', start_time);
+  localStorage.setItem('end_time', end_time);
   localStorage.setItem('people', people);
   localStorage.setItem('selected', selected);
-  var data = {start_date: start_date, end_date: end_date, time: time, people: people, selected: selected};    
-
+  var data = {start_date: start_date, end_date: end_date, start_time: start_time, end_time: end_time, people: people, selected: selected};    
   var request = $.ajax({
     type: form.prop('method'),
     url: window.location.protocol+'//'+window.location.host+"/scrape",
