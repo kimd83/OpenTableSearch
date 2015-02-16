71814eb5379161f07e585636a6a1a630be39e5da
David Kim <kimd83@gmail.com>
2015-02-16 15:38:02 -0500

    move to postgresql

---
+++ app/models.py
@@ -0,0 +1,31 @@
+from . import db
+
+class Alert(db.Model):
+    __tablename__ = 'alerts'
+    id = db.Column(db.Integer, primary_key=True)
+    rid = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    start_date = db.Column(db.String(64))
+    end_date = db.Column(db.String(64))
+    start_time = db.Column(db.String(64))
+    end_time = db.Column(db.String(64))
+    people = db.Column(db.String(64))
+    email = db.Column(db.String(64))
+    status = db.Column(db.String(64))
+
+    def __repr__(self):
+        return '<Alert %r>' % self.email
+
+
+class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    opentable_id = db.Column(db.Integer, unique=True)
+    alerts = db.relationship('Alert', backref='restaurant', lazy= "dynamic")
+
+    def __repr__(self):
+        return '<Restaurant %r>' % self.name
+
+
+
+
