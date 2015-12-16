1c457dbb46cd0e557eef00b739119845eda53ed1
David Kim <kimd83@gmail.com>
2015-12-16 00:02:53 -0500

    add db.app=app in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+    db.app = app
     db.init_app(app)
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
